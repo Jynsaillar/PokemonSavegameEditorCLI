bc96e3f25b02d1ce361b7749e7fd92cbcfee4738
Jynsaillar <jynsaillar@ymail.com>
2019-03-24 02:15:07 +0100

    Added TrainerInfo.SecurityKey implementation and finished the ReadFromBinary(...) function (for now).

--- SectionDataTypes/TrainerInfo.cs
+++ SectionDataTypes/TrainerInfo.cs
@@ -11,12 +11,26 @@ namespace PokemonSaves
         private TrainerId _trainerID;
         private TimePlayed _timePlayed;
         private Options _options;
+        private uint _gameCode;
+        private uint _securityKey;
         public long PlayerName { get => _playerName; set => _playerName = value; }
         public byte PlayerGender { get => _playerGender; set => _playerGender = value; }
         public TrainerId TrainerID { get => _trainerID; set => _trainerID = value; }
         public TimePlayed TimePlayed { get => _timePlayed; set => _timePlayed = value; }
         public Options Options { get => _options; set => _options = value; }
-        // TODO: Add missing fields & corresponding properties for GameCode, SecurityKey
+
+        /// <summary> 
+        /// <para/>Ruby, Sapphire = 0x00000000
+        /// <para/>FireRed, LeafGreen = 0x00000001
+        /// <para/>Emerald = any offset not 0x00000000 or 0x00000001, determines the offset for the security key as well.
+        /// </summary>
+        public uint GameCode { get => _gameCode; set => _gameCode = value; }
+        /// <summary>
+        /// <para/>Emerald is the only Generation III game in which the security key is utilized.
+        /// <para/>If used, it encrypts values like money and item quantities by simply running an XOR operation with the target bytes.
+        /// <para/>Obtain the decrypted values by simply running an XOR operation with the security key again.
+        /// </summary>
+        public uint SecurityKey { get => _securityKey; set => _securityKey = value; }
         public enum Offsets : long
         {
             PlayerName = 0x0000,
@@ -58,6 +72,12 @@ namespace PokemonSaves
                 Options = new Options();
             }
             Options.ReadFromBinary(binaryReader);
+            // GameCode
+            binaryReader.BaseStream.Seek(startOffset + (long)Offsets.GameCode, SeekOrigin.Begin);
+            GameCode = binaryReader.ReadUInt32();
+            // SecurityKey
+            binaryReader.BaseStream.Seek(startOffset + (long)Offsets.SecurityKey, SeekOrigin.Begin);
+            SecurityKey = binaryReader.ReadUInt32();
         }
 
     }
