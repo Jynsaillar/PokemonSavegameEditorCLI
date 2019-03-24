1ec854e2215513729cb6185d9b519f368dd48d6c
Jynsaillar <jynsaillar@ymail.com>
2019-03-24 19:09:46 +0100

    Added IBinaryParsable interface to SaveDataSection and implemented SaveDataSection.ReadFromBinary(...); Added first switch case for SectionID = TrainerInfo; Parsing TrainerInfo from an actual save file should now theoretically be possible;

--- SaveStructures/SaveDataSection.cs
+++ SaveStructures/SaveDataSection.cs
@@ -1,13 +1,15 @@
+using System.IO;
+
 namespace PokemonSaves
 {
-    public class SaveDataSection : IDataSection
+    public class SaveDataSection : IDataSection, IBinaryParsable
     {
         SectionData _data;
-        ushort _sectionId;
+        DataSectionTypes _sectionId;
         short _checksum;
         uint _saveIndex;
         public SectionData Data { get => _data; set => _data = value; }
-        public ushort SectionID { get => _sectionId; set => _sectionId = value; }
+        public DataSectionTypes SectionID { get => _sectionId; set => _sectionId = value; }
         public short Checksum { get => _checksum; set => _checksum = value; }
         public uint SaveIndex { get => _saveIndex; set => _saveIndex = value; }
 
@@ -18,12 +20,44 @@ namespace PokemonSaves
             Checksum = 0x0FF6,
             SaveIndex = 0x0FFC
         }
+
+        public void ReadFromBinary(BinaryReader binaryReader)
+        {
+            long startOffset = binaryReader.BaseStream.Position;
+            // SectionID
+            binaryReader.BaseStream.Seek(startOffset + (long)Offsets.SectionID, SeekOrigin.Begin);
+            SectionID = (DataSectionTypes)binaryReader.ReadUInt16();
+            // Checksum
+            binaryReader.BaseStream.Seek(startOffset + (long)Offsets.Checksum, SeekOrigin.Begin);
+            Checksum = binaryReader.ReadInt16();
+            // SaveIndex
+            binaryReader.BaseStream.Seek(startOffset + (long)Offsets.SaveIndex, SeekOrigin.Begin);
+            SaveIndex = binaryReader.ReadUInt32();
+
+            // Parsing of actual SectionData starts here since the SectionID is needed to determine the derived
+            // type of SectionData (e.g. TrainerInfo, GameState, RivalInfo etc.).
+            switch (SectionID)
+            {
+                case DataSectionTypes.TrainerInfo:
+                    binaryReader.BaseStream.Seek(startOffset + (long)Offsets.Data, SeekOrigin.Begin); // Seeks start offset of SectionData.
+                    var trainerInfo = new TrainerInfo();
+                    trainerInfo.ReadFromBinary(binaryReader); // Parses SectionData as TrainerInfo since the SectionID matches TrainerInfo.
+                    Data = trainerInfo; // Box TrainerInfo into SectionData type.
+                    /* Since reading the SectionData block's actual data section is finished but the stream is not at the end offset
+                       of the total SectionData block, the stream position needs to be updated before proceeding/exiting the function
+                       to prevent breaking future read operations. */
+                    uint sectionSize = SectionsHelper.GetSectionSize(SectionID);
+                    binaryReader.BaseStream.Seek(startOffset + sectionSize, SeekOrigin.Begin);
+                    break;
+                    // TODO: Implement cases for remaining DataSectionTypes.
+            }
+        }
     }
 
     interface IDataSection
     {
         SectionData Data { get; set; }
-        ushort SectionID { get; set; }
+        DataSectionTypes SectionID { get; set; }
         short Checksum { get; set; }
         uint SaveIndex { get; set; }
     }
