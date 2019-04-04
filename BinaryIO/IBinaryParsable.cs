92272155d7beacb1413e85b494c2634bb88a8208
Jynsaillar <jynsaillar@ymail.com>
2019-04-05 00:59:20 +0200

    Added StartOffset (long) as required property to IBinaryParsable interface (which will later enable calculating property offsets for writing modified property data to a save file copy).

--- BinaryIO/IBinaryParsable.cs
+++ BinaryIO/IBinaryParsable.cs
@@ -2,6 +2,7 @@ namespace PokemonSaves
 {
     interface IBinaryParsable
     {
+        long StartOffset { get; set; }
         void ReadFromBinary(System.IO.BinaryReader binaryReader, GameIDs gameID);
     }
 }
