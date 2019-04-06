d3ba13baf66c1db3786f42407497a41b729a0dfb
Jynsaillar <jynsaillar@ymail.com>
2019-04-06 03:42:20 +0200

    Added constructor to SectionData that takes a SaveDataSection as parameter to allow derived SectionData types to access SaveDataSection checksum operations.

--- SaveStructures/SectionData.cs
+++ SaveStructures/SectionData.cs
@@ -3,5 +3,11 @@ namespace PokemonSaves
 {
     public class SectionData
     {
+        private SaveDataSection _saveDataSection;
+        public SaveDataSection SaveDataSection { get => _saveDataSection; set => _saveDataSection = value; }
+        public SectionData(SaveDataSection saveDataSection)
+        {
+            SaveDataSection = saveDataSection;
+        }
     }
 }
