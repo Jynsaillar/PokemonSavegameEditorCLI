cdd229dd3bf9d01791d21082e38f6e2b1466748b
Jynsaillar <jynsaillar@ymail.com>
2019-05-04 01:44:40 +0200

    Added method for extracting IVs and egg state from an egg/IV container type as named tuple.

--- SaveStructures/PokemonDataHelper.cs
+++ SaveStructures/PokemonDataHelper.cs
@@ -13,5 +13,19 @@ namespace PokemonSaves
         {
             return OTID ^ personalityValue;
         }
+
+        public static (byte HPIV, byte AttackIV, byte DefenseIV, byte SpeedIV, byte SpecialAttackIV, byte SpecialDefenseIV, byte IsEgg, byte Ability) ExtractIVs(Miscellaneous miscellaneous)
+        {
+            var ivsEggAndAbility = miscellaneous.IVsEggAndAbility;
+            byte hpIV = (byte)((ivsEggAndAbility >> 0) & 0x1F);
+            byte attackIV = (byte)((ivsEggAndAbility >> 5) & 0x1F);
+            byte defenseIV = (byte)((ivsEggAndAbility >> 10) & 0x1F);
+            byte speedIV = (byte)((ivsEggAndAbility >> 15) & 0x1F);
+            byte specialAttackIV = (byte)((ivsEggAndAbility >> 20) & 0x1F);
+            byte specialDefenseIV = (byte)((ivsEggAndAbility >> 25) & 0x1F);
+            byte isEgg = (byte)((ivsEggAndAbility >> 30) & 1);
+            byte ability = (byte)((ivsEggAndAbility >> 31) & 1);
+            return (hpIV, attackIV, defenseIV, speedIV, specialAttackIV, specialDefenseIV, isEgg, ability);
+        }
     }
 }
